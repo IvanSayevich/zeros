5e1af3f0af67265958d9f35aa1ebdc5b802d2511
Ivan <sundial@tut.by>
2018-02-24 09:10:35 +0300

    zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let max = number;
+  let zeroCount = 0;
+  function sum(num){
+    let x = num;
+    if (x%5 === 0) {
+      zeroCount+=1;
+      x= x/5;
+      sum(x);  
+    }
+
+  }
+for (i=1;i<=max; i++){
+  sum(i);
 }
+  return zeroCount;
+}
+
+
